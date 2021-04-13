627ea37ab07e7b5ff75201199fefd171187de3a0
Rx JoseM <51371604+JOS3GAMER1@users.noreply.github.com>
2021-04-13 11:07:43 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,18 @@
+function a1() {
+  document.getElementById("a1").innerHTML = "hey!";
+}
+function a2() {
+  document.getElementById("a2").innerHTML = "hey!";
+}
+function a3() {
+  document.getElementById("a3").innerHTML = "hey!";
+}
+function z1() {
+  document.getElementById("z1").innerHTML = "uagh!";
+}
+function z2() {
+  document.getElementById("z2").innerHTML = "uagh!";
+}
+function z3() {
+  document.getElementById("z3").innerHTML = "uagh!";
+}
